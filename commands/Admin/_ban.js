f35b8a53bf3014d2183357e19f06e1afa30cdbb1
UntoldHacker-Dev <74806602+Untoldhacker-Dev@users.noreply.github.com>
2021-10-17 15:38:48 +0530

    Reduced itration spends shifted to Bot.blockChat()

--- commands/Admin/_ban.js
+++ commands/Admin/_ban.js
@@ -13,7 +13,7 @@ if(!isAdmin()){return}
 if (user.telegramid == message) {
   Bot.sendMessage("Error: 404, User is not found!")
   return
-} else Bot.setProperty(message + "ban", true)
+} else Bot.blockChat(message)
 Bot.sendMessage(
   "Successfully banned: " +
     "[" +
