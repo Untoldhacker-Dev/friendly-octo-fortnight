7c745c07283899960e8f33c9f65a1530033b4c10
UntoldHacker-Dev <74806602+Untoldhacker-Dev@users.noreply.github.com>
2021-10-19 08:54:43 +0530

    Update @.js

--- commands/@.js
+++ commands/@.js
@@ -8,7 +8,9 @@
   keyboard: 
   aliases: 
 CMD*/
-
+var currency = Bot.getProperty("curency_Name")
+var balance = Libs.ResourcesLib.userRes("balance")
+var wallet = User.getProperty("wallet")
   var admin_chat = Bot.getProperty("admin_chat")
   if ( command.folder == "Admin" && admin_chat =! user.telegramid ){
     return 
